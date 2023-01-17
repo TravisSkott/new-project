9226cb7939b0dc5c6e67ec81e1abd494799d3a03
TravisSkott <danik.ray.2016@mail.ru>
2023-01-17 18:13:48 +0300

    created drop image

--- js.js
+++ js.js
@@ -1,1 +1,26 @@
-console.log("hi daniil ")
+const zone1 = document.querySelector(".zone-1")
+const zone2 = document.querySelector(".zone-2")
+let ufo = document.getElementById("ufo")
+
+zone1.ondragover = allowDrop;
+zone2.ondragover = allowDrop;
+
+function allowDrop(event){
+    event.preventDefault();
+}
+
+ufo.ondragstart = drag;
+
+function drag(event){
+    event.dataTransfer.setData("id",event.target.id)
+}
+
+zone1.ondrop = drop;
+zone2.ondrop = drop;
+
+
+function drop(event){
+    let itemId = event.dataTransfer.getData("id")
+    console.log(itemId)
+    event.target.append(document.getElementById(itemId))
+}
